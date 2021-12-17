edc266cf9d03caf58800c137b14ebc1f95a1004d
Komarov Maksim <mx123@yandex.ru>
2021-12-17 18:40:35 +1000

    Add method to BrandRepository interface

--- shop-database/src/main/java/ru/mskomarov/persist/repo/BrandRepository.java
+++ shop-database/src/main/java/ru/mskomarov/persist/repo/BrandRepository.java
@@ -1,7 +1,10 @@
 package ru.mskomarov.persist.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.mskomarov.persist.model.Brand;
 
 public interface BrandRepository extends JpaRepository<Brand, Long> {
+    @Query("from Brand b where name = :name")
+    Brand findOneByName(String name);
 }
